985e5c9f8f15e37ff316d0c6bf3d973623eb166e
Rinrodan <109042144+Rinrodan@users.noreply.github.com>
2023-09-07 13:08:45 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.rinrodan.github.io/portfolio/',
+    site: 'https://www.rinrodan.github.io',
+    base: '/portfolio',
     output: 'static',
     outDir: './dist',
     build: {
